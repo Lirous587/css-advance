7f774a758d67e7cfab8dcd2de96902e95fad834f
lzh <2115883273@qq.com>
2025-03-09 09:37:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="g-container">
+      <ul>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
